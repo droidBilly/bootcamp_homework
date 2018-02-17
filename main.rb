8ee1fa1d03983ab137f22cfc337dd8ef1c740e0e
Peter Schrammen <b.schrammen@gmail.com>
2018-02-17 15:00:33 +0100

    method for subcollection for contacts with .org mailaddresses

--- main.rb
+++ main.rb
@@ -14,5 +14,18 @@ def copy_contacts_from_usa
   read_contacts.select{|contact| contact[:phone] =~ /^[+]1/} #regex for starts with +1
 end
 
+def copy_contacts_with_org
+  read_contacts.select{|contact| contact[:email] =~ /[.][o][r][g]/} #regex for starts with +1
+end
+
+
+puts "Excercise 1:"
 puts copy_contacts_names_and_phones.inspect
+puts "--------------------"
+puts "Excercise 2.1:"
 puts copy_contacts_from_usa
+puts "--------------------"
+puts "Excercise 2.2:"
+puts copy_contacts_with_org
+puts "--------------------"
+puts "Excercise 3:"
