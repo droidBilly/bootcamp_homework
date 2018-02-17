1ad699a30acf630b8910d5431cf8e21571b6504c
Peter Schrammen <b.schrammen@gmail.com>
2018-02-18 00:18:44 +0100

    refactors code, adds messages to the output of each method

--- main.rb
+++ main.rb
@@ -18,14 +18,30 @@ def copy_contacts_with_org
   read_contacts.select{|contact| contact[:email] =~ /[.][o][r][g]/} #regex for starts with +1
 end
 
+def count_contacts_from_usa
+  copy_contacts_from_usa.reduce(0) {|counter|
+    counter += 1
+  }
+end
 
-puts "Excercise 1:"
+def count_contacts_with_org
+  copy_contacts_with_org.reduce(0) {|counter|
+    counter += 1
+  }
+end
+
+puts "Excercise 1:(Copy of Contacts array which holds name as key and phone to value)"
 puts copy_contacts_names_and_phones.inspect
 puts "--------------------"
-puts "Excercise 2.1:"
+puts "Excercise 2.1:(Shows only contacts where phone starts with '+1')"
 puts copy_contacts_from_usa
 puts "--------------------"
-puts "Excercise 2.2:"
+puts "Excercise 2.2:(Shows only contacts where email contains '.org')"
 puts copy_contacts_with_org
 puts "--------------------"
-puts "Excercise 3:"
+puts "Excercise 3:1(Counts contacts with phone starts with '+1')"
+puts count_contacts_from_usa
+puts "--------------------"
+puts "Excercise 3:2(Counts contacts with emails contain '.org')"
+puts count_contacts_with_org
+puts "--------------------"
