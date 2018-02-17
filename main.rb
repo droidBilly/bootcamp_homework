494dfc90b9544db83ad234b5c604d1173413ac81
Peter Schrammen <b.schrammen@gmail.com>
2018-02-17 13:49:51 +0100

    Method for mapping contacts to new array with contachashes

--- main.rb
+++ main.rb
@@ -5,3 +5,10 @@ def read_contacts
   contacts_json = File.read("contacts.json")
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
+
+def copy_contacts_names_and_phones
+  contact_hash = Hash.new
+  result = read_contacts.map {|contact| contact_hash = { "#{contact[:name]}": "#{contact[:phone]}"}}
+end
+
+puts result = copy_contacts_names_and_phones
